931fb82019987f2deb3f3f2ec1a20efa6d823695
4Nune4 <jorgeenrrique123456789@gmail.com>
2024-02-04 09:06:03 -0500

    primera prueba

---
+++ cjs/index.js
@@ -0,0 +1,4 @@
+//! llamando a la funcion del archivo sum
+const {suma} = require ('./sum.js')
+//! la operacion 
+console.log(suma(4,8))
---
+++ cjs/sum.js
@@ -0,0 +1,10 @@
+
+//! la funcion se encuentra en este archivo sum.js
+//! y se exporta con el nombre de la funcion 
+function suma (a, b){
+    return(a * b)
+}
+//! la funcion que permite exportar
+module.exports={
+    suma
+}
